cd916bd09dc8b2608688ebf4d84fde2c1bc50d11
mgame <mgame@DESKTOP-STIBQ4U>
2022-09-20 20:33:48 +0300

    Kodlama.io Java 2022 ilk hafta ödevi

---
+++ DataTypes/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		double sayi = 12.5;
+		System.out.println(sayi);
+		sayi = -129;
+		System.out.println(sayi);
+		
+		char karakter = 'A';
+		System.out.println(karakter);
+		
+		boolean dogruMu = false;
+		System.out.println(dogruMu);
+
+	}
+
+}
---
+++ miniProjeSayiBulma/src/miniProjeSayiBulma/Main.java
@@ -0,0 +1,19 @@
+package miniProjeSayiBulma;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] sayilar= {1,2,5,7,9,0};
+		int aranacak=3;
+		
+		for(int sayi:sayilar) {
+			if(aranacak==sayi) {
+				System.out.println("Sayı Bulundu.");
+				return;
+			}
+		}
+		System.out.println("Sayı Bulunamadı.");
+
+	}
+
+}
